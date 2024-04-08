6fbf972605506c62666a8ebf4a98a420ca9ad6ff
Aliaksandr Pyrkh <alexpyrkh@gmail.com>
2024-04-09 01:59:37 +0200

    Added garbage-day

---
+++ .config/nvim/lua/apyrkh/plugins/garbage-day.lua
@@ -0,0 +1,9 @@
+-- Garbage collector that stops inactive LSP clients to free RAM
+return {
+  "zeioth/garbage-day.nvim",
+  dependencies = "neovim/nvim-lspconfig",
+  event = "VeryLazy",
+  opts = {
+    -- your options here
+  }
+}
