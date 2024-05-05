76e6dd9570d9a62dd796b490e9b91a20b18ff0b2
Aliaksandr Pyrkh <alexpyrkh@gmail.com>
2024-05-05 17:33:36 +0200

    Made all plugins to have its own config file

--- .config/nvim/lua/apyrkh/plugins/init.lua
+++
@@ -1,8 +0,0 @@
-return {
-  -- A port of Matt Pocock's ts-error-translator for VSCode for turning messy
-  -- and confusing TypeScript errors into plain English.
-  "dmmulroy/ts-error-translator.nvim",
-
-  -- Fun visual effects in buffer
-  { "eandrju/cellular-automaton.nvim", event = "VeryLazy" }
-}
