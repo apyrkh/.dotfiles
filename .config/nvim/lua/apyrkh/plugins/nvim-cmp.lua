893887d85a09c306386313acbeaa7c669f825a0d
Aliaksandr Pyrkh <alexpyrkh@gmail.com>
2024-05-05 17:40:43 +0200

    Updated luasnip config

--- .config/nvim/lua/apyrkh/plugins/nvim-cmp.lua
+++ .config/nvim/lua/apyrkh/plugins/nvim-cmp.lua
@@ -10,8 +10,12 @@ return {
   dependencies = {
     "hrsh7th/cmp-buffer", -- source for text in buffer
     "hrsh7th/cmp-path", -- source for file system paths
+    {
+      "L3MON4D3/LuaSnip",
+      version = "v2.*",
+      build = "make install_jsregexp",
+    },
     "saadparwaiz1/cmp_luasnip", -- source for luasnip
-    "L3MON4D3/LuaSnip", -- snippet engine
     "rafamadriz/friendly-snippets", -- useful snippets
     "onsails/lspkind.nvim", -- vscode-like pictograms for neovim lsp completion items
   },
