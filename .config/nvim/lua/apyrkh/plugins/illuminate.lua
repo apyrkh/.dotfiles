077471ec2aeb9386731456c82c81cc92059d8de9
Aliaksandr Pyrkh <alexpyrkh@gmail.com>
2024-08-18 17:15:35 +0200

    Updated illuminate config

--- .config/nvim/lua/apyrkh/plugins/illuminate.lua
+++ .config/nvim/lua/apyrkh/plugins/illuminate.lua
@@ -2,6 +2,13 @@ return {
   "RRethy/vim-illuminate",
   event = "VeryLazy",
   config = function()
-    require("illuminate").configure()
+    require("illuminate").configure({
+      providers = {
+          'lsp',
+          'treesitter',
+          -- 'regex',
+      },
+      under_cursor = false,
+    })
   end,
 }
