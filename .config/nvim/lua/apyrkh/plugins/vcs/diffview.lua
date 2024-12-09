661a61369551e617ede67b4f3b054e7b19402a9b
Aliaksandr Pyrkh <alexpyrkh@gmail.com>
2024-12-09 23:56:47 +0100

    Actualized diffview

--- .config/nvim/lua/apyrkh/plugins/vcs/diffview.lua
+++ .config/nvim/lua/apyrkh/plugins/vcs/diffview.lua
@@ -1,7 +1,10 @@
+-- Provides a Git diff UI for Neovim, with features like file history and branch comparison
+-- #vcs #git #diff #ui
 return {
   "sindrets/diffview.nvim",
   config = function ()
     require("diffview").setup({
+      enhanced_diff_hl = true, -- Highlight word-level changes in diffs
       signs = {
         fold_closed = "",
         fold_open = "",
