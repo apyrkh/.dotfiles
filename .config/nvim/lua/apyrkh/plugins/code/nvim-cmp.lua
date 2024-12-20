166ece6e786e3714855e254b737bc6af414a3c06
Aliaksandr <alexpyrkh@gmail.com>
2024-12-20 14:04:01 +0100

    Update nvim-cmp config

--- .config/nvim/lua/apyrkh/plugins/code/nvim-cmp.lua
+++ .config/nvim/lua/apyrkh/plugins/code/nvim-cmp.lua
@@ -31,10 +31,26 @@ return {
     require("luasnip.loaders.from_vscode").lazy_load()
 
     cmp.setup({
-      -- @TODO: probably this block can be removed, keep it for some time
-      -- completion = {
-      --   completeopt = "menu,menuone,preview,noselect",
-      -- },
+      window = {
+        completion = cmp.config.window.bordered(),
+        documentation = cmp.config.window.bordered(),
+      },
+
+      sources = cmp.config.sources(
+        {
+          { name = "nvim_lsp", max_item_count = 10 },
+          { name = "luasnip",  max_item_count = 10 },
+          { name = "buffer",   max_item_count = 10 }, -- text within current buffer
+          { name = "path",     max_item_count = 10 }, -- file system paths
+        }
+      ),
+
+      formatting = {
+        format = lspkind.cmp_format({
+          maxwidth = 50,
+          ellipsis_char = "...",
+        }),
+      },
 
       -- configure how nvim-cmp interacts with snippet engine
       snippet = {
@@ -44,51 +60,37 @@ return {
       },
 
       mapping = {
-        -- @TODO: this is not needed as it provoke enter key
-        -- complete selection
-        -- ["<CR>"] = cmp.mapping({
-        --   i = function(fallback)
-        --     if cmp.visible() and cmp.get_active_entry() then
-        --       cmp.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = false })
-        --     else
-        --       fallback()
-        --     end
-        --   end,
-        --   s = cmp.mapping.confirm({ select = true }),
-        --   c = cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = true }),
-        -- }),
-        -- go next
-        -- if there is only one possible completion, it will be completed immediately
-        ["<Tab>"] = cmp.mapping({
-          i = function(fallback)
-            if cmp.visible() then
-              if #cmp.get_entries() == 1 then
-                cmp.confirm({ select = true })
-              else
-                cmp.select_next_item()
-              end
-            elseif has_words_before() then
-              cmp.complete()
-              if #cmp.get_entries() == 1 then
-                cmp.confirm({ select = true })
-              end
-            else
-              fallback()
-            end
-          end,
-        }),
-        -- go previous
-        ["<S-Tab>"] = cmp.mapping({
-          i = function(fallback)
-            if cmp.visible() then
-              cmp.select_prev_item()
-            elseif has_words_before then
-              cmp.complete()
-            else
-              fallback()
-            end
+        -- confirm selection
+        ["<CR>"] = cmp.mapping(function(fallback)
+          if cmp.visible() and cmp.get_active_entry() then
+            cmp.confirm({ behavior = cmp.ConfirmBehavior.Replace })
+          else
+            fallback()
           end
-        }),
+        end, { "i", "s", "c" }),
+
+        -- go next
+        ["<Tab>"] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_next_item({ behavior = cmp.ConfirmBehavior.Replace })
+          elseif has_words_before then
+            cmp.complete()
+          else
+            fallback()
+          end
+        end, { "i", "s", "c" }),
+
+        -- go previous
+        ["<S-Tab>"] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_prev_item({ behavior = cmp.ConfirmBehavior.Replace })
+          elseif has_words_before then
+            cmp.complete()
+          else
+            fallback()
+          end
+        end, { "i", "s", "c" }),
+
         -- scroll docs top
         ["<C-e>"] = cmp.mapping({
           i = function(fallback)
@@ -97,6 +99,7 @@ return {
             end
           end
         }),
+
         -- scroll docs bottom
         ["<C-y>"] = cmp.mapping({
           i = function(fallback)
@@ -105,6 +108,7 @@ return {
             end
           end
         }),
+
         -- cancel autocompletion
         ["<C-c>"] = cmp.mapping({
           i = function(fallback)
@@ -112,22 +116,6 @@ return {
               fallback()
             end
           end
-        }),
-      },
-
-      sources = cmp.config.sources(
-        {
-          { name = "nvim_lsp" },
-          { name = "luasnip" }, -- snippets
-          { name = "buffer" },  -- text within current buffer
-          { name = "path" },    -- file system paths
-        }
-      ),
-
-      formatting = {
-        format = lspkind.cmp_format({
-          maxwidth = 50,
-          ellipsis_char = "...",
         }),
       },
     })
