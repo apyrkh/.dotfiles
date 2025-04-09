afd35317d0781ca53a3ddfa70d9c94817ca6fd6c
Aliaksandr Pyrkh <alexpyrkh@gmail.com>
2025-04-09 10:48:03 +0200

    Added copilot signin/signout keymaps

--- .config/nvim/lua/apyrkh/plugins/lsp/copilot.lua
+++ .config/nvim/lua/apyrkh/plugins/lsp/copilot.lua
@@ -4,6 +4,7 @@ return {
     init = function()
       vim.g.copilot_enabled = false
 
+
       vim.keymap.set("n", "<leader>cc", function()
         if vim.g.copilot_enabled == true then
           vim.cmd("Copilot disable")
@@ -15,6 +16,16 @@ return {
           vim.notify("Copilot enabled", vim.log.levels.INFO)
         end
       end, { desc = "Toggle Copilot" })
+
+      vim.keymap.set("n", "<leader>cs", function()
+        vim.cmd("Copilot setup")
+        vim.notify("Copilot setup", vim.log.levels.INFO)
+      end, { desc = "Setup Copilot" })
+
+      vim.keymap.set("n", "<leader>cq", function()
+        vim.cmd("Copilot signout")
+        vim.notify("Copilot signout", vim.log.levels.INFO)
+      end, { desc = "Signout Copilot" })
     end,
   }
 }
