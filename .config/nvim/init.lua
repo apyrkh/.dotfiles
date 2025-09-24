fa689c49446b21dc55915b5b1ae213fd95ef4c18
Aliaksandr Pyrkh <alexpyrkh@gmail.com>
2025-09-24 13:33:55 +0200

    Introduced workspace settings possibility

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,6 +1,7 @@
-require("apyrkh.settings")
 require("apyrkh.keymaps")
-require("apyrkh.lazy")
+require("apyrkh.settings")
+require("apyrkh.workspace")
 
+require("apyrkh.lazy")
 require("apyrkh.keymaps_plugins")
 require("apyrkh.settings_plugins")
