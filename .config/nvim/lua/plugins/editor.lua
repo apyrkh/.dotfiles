a1271daaefde63d720dd8c90f3451fb40aa6d34f
Aliaksandr Pyrkh <alexpyrkh@gmail.com>
2026-01-29 13:48:19 +0100

    Improved Oil.nvim config

--- .config/nvim/lua/plugins/editor.lua
+++ .config/nvim/lua/plugins/editor.lua
@@ -7,30 +7,26 @@ return {
       { "-", "<CMD>Oil<CR>", desc = "Open parent directory" },
     },
     opts = {
-      default_file_explorer = true,
-      delete_to_trash = true,
-      -- skip_confirm_for_simple_edits = true,
       keymaps = {
-        ["g?"] = "actions.show_help",
-        ["<CR>"] = "actions.select",
-        ["<C-s>"] = false, -- { "actions.select", opts = { vertical = true } },
-        ["<C-h>"] = false, -- { "actions.select", opts = { horizontal = true } },
-        ["<C-t>"] = false, -- { "actions.select", opts = { tab = true } },
-        ["<C-p>"] = "actions.preview",
-        ["<C-c>"] = "actions.close",
-        ["<C-l>"] = false, -- "actions.refresh",
-        ["<C-r>"] = "actions.refresh",
-        ["-"] = { "actions.parent", mode = "n" },
-        ["_"] = { "actions.open_cwd", mode = "n" },
-        ["`"] = { "actions.cd", mode = "n" },
-        ["g~"] = { "actions.cd", opts = { scope = "tab" }, mode = "n" },
-        ["gs"] = { "actions.change_sort", mode = "n" },
-        ["gx"] = "actions.open_external",
-        ["g."] = { "actions.toggle_hidden", mode = "n" },
-        ["g\\"] = false, -- { "actions.toggle_trash", mode = "n" },
+        ["<C-v>"] = { "actions.select", opts = { vertical = true } },
+        ["<C-s>"] = { "actions.select", opts = { horizontal = true } },
+        ["<C-h>"] = false,
       },
       view_options = {
         show_hidden = true,
+        is_hidden_file = function(name, bufnr)
+          -- default
+          -- local m = name:match("^%.")
+          -- return m ~= nil
+
+          local hidden_files = {
+            ["build"] = true,
+            ["dist"] = true,
+            ["node_modules"] = true,
+            [".git"] = true,
+          }
+          return hidden_files[name] ~= nil
+        end,
         is_always_hidden = function(name)
           local hidden_names = {
             ".DS_Store",
